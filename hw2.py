5ee12cb93a9ca867f26def1d900e753e4405932a
DaniRabi <danirabi98@gmail.com>
2019-05-12 18:16:16 +0300

    updated to dictionary implementation also, it's called "input.txt" now

--- hw2.py
+++ hw2.py
@@ -1,17 +1,18 @@
+def is_staff_choice(line, faculty):
+    # if the given line is a staff choice line of given faculty
+    return "staff choice" in line and faculty in line
+
+
 def inside_contest(faculty, file_name):
     f = open(file_name, 'r')
     lines = f.readlines()
     f.close()
 
-    programs = []
+    programs = {}
     for line in lines:
         if is_staff_choice(line, faculty):
-            programs = get_programs(line)
+            programs = get_programs_2(line)
             # break is not needed under the assumption there's only one staff choice for the faculty
-
-    # first faculty starts with 20 points (staff's choice)
-    # all the rest are initialized to 0
-    program_points = [20] + [0] * (len(programs) + 1)
 
     voters = []     # IDs of students who voted already
     for line in lines:
@@ -22,35 +23,35 @@ def inside_contest(faculty, file_name):
             if student_id not in voters:    # check the student hasn't voted already
                 voters.append(student_id)
                 program = words[3]
-                index = programs.index(program)
-                program_points[index] += 1
+                programs[program] += 1      # add a vote = 1 point
 
-    # get the index of the program that got the most points
-    max_points = max(program_points)
-    max_index = program_points.index(max_points)
-
-    return programs[max_index]  # return program in that index
+    return get_max_key(programs)   # return program with max points
 
 
 def get_programs(line):
-    # initialize this faculty's programs list
-    # Assumptions:
-    # 1) only one staff choice line per faculty
-    # 2) there are spaces ONLY between parameters (and in "staff choice")
-
+    """
+    Initialize this faculty's programs list
+    Assumptions:
+    1) only one staff choice line per faculty
+    2) there are spaces ONLY between parameters (and in "staff choice")
+    """
     words = line.split(' ')
     # words = ['staff', 'choice', <program 1>, ..., <program n>, <faculty>]
 
-    programs = []
-    for i in range(2, len(words) - 1):
-        programs.append(words[i])
+    # first faculty starts with 20 points (staff's choice)
+    programs = {words[2]: 20}
+    for i in range(3, len(words) - 1):
+        programs[words[i]] = 0  # all the rest are initialized to 0
     return programs
 
 
-def is_staff_choice(line, faculty):
-    # if the given line is a staff choice line of given faculty
-    return "staff choice" in line and faculty in line
+def get_max_key(dict):
+    program_points = list(dict.values())
+    max_points = max(program_points)
+    max_index = program_points.index(max_points)
+
+    return list(dict.keys)[max_index]
 
 
-winner = inside_contest("ComputerScience", "vote_commands.txt")
+winner = inside_contest("ComputerScience", "input.txt")
 print(winner)
