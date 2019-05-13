ca7685ff3fa03349bc451c5be597f9390d5a1301
Eyal <eyal.kushnir@campus.technion.ac.il>
2019-05-13 08:58:11 +0300

    a

--- hw2.py
+++ hw2.py
@@ -16,12 +16,18 @@ def inside_contest(faculty, file_name):
     lines = f.readlines()
     f.close()
 
+    # iterate on the file's lines, find a line with "staff choise" and the given faculty.
+    # get_programs() transform the line to a dictionary with the faculty's /
+    # programs as keys and num of points as data
     programs = {}
     for line in lines:
         if "staff choice" in line and faculty in line:
             programs = get_programs(line)
             # break is not needed under the assumption there's only one staff choice for the faculty
 
+    # iterate again on the file's line, find lines with "inside contest" and the given faculty
+    # update the 'programs' points dictionary with the students votes
+    # each student votes ones
     voters = []     # IDs of students who voted already
     for line in lines:
         if "inside contest" in line and faculty in line:
@@ -63,6 +69,8 @@ def get_max_program(dict):
                  to its amount of points
     :return: The name of the study program with the highest amount of points
     """
+
+    # find max value in the points dictionary and return it's key (name of program)
     program_points = list(dict.values())
     max_points = max(program_points)
     max_index = program_points.index(max_points)
@@ -71,13 +79,19 @@ def get_max_program(dict):
 
 
 """************************************** PROGRAM **********************************"""
+# create new Techniovision
 t = Techniovision.TechniovisionCreate()
 
+# get the lines from the given input file
 file_name = "input.txt"
 file = open(file_name, 'r')
 lines = file.readlines()
 file.close()
 
+# iterate on the file's lines. for each line with "staff choise" /
+# get faculty's chosen program with inside_contest() /
+# save in 'faculties' dictionary with the chosen programs  /
+# as keys and faculties names as data
 faculties = {}
 for line in lines:
     if "staff choice" in line:
@@ -86,13 +100,14 @@ for line in lines:
         program = inside_contest(faculty, file_name)
         faculties[program] = faculty
 
+# iterate again on the file's lines. for each line with "techniovision" /
+# update the Techniovision votes count with the outside functions.
 for line in lines:
     if "techniovision" in line:
         words = line.split()
         # words = ["techniovision", <student_id>, <study_program>, <student_faculty>]
 
         student_id = words[1]
-
         study_program = words[2]
         student_faculty = words[3]
 
